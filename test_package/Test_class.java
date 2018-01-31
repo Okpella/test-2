9432a37f9bbc9c14e743102360e9cba831bfb372
Jerry <abouncej@gmail.com>
2018-01-31 14:48:26 +0100

    my first java work

---
+++ test_package/Test_class.java
@@ -0,0 +1,15 @@
+package test_package;
+
+public class Test_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("This is a test class");
+	String message;
+	message ="Prof";
+	System.out.println(message);
+
+	}
+
+}
